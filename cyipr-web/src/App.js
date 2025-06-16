56326f77c46b1ae06614db9a4e5fe6aecc60d547
nayamiskr <bigbird0887@gmail.com>
2025-06-16 17:36:30 +0800

    Cleaned up nested git

---
+++ cyipr-web/src/App.js
@@ -0,0 +1,13 @@
+import './style.css';
+import nav from './components/Navbar.js';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Cyiprhhh Web Application</h1>
+      <nav />
+    </div>
+  );
+}
+
+export default App;

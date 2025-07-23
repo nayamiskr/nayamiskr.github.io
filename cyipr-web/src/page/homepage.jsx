93e73262510796e067ffb6f14381a743670ab115
nayamiskr <bigbird0887@gmail.com>
2025-07-23 13:12:47 +0800

    設計homepage （未完）

--- cyipr-web/src/page/homepage.jsx
+++ cyipr-web/src/page/homepage.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import './homepage.css';
 import Navbar from "../components/Navbar";
 
-function Homepage () {
+function Homepage() {
     return (
         <div class="homepage">
-        <Navbar/>
+            <Navbar />
             <div class="content">
-                <h1>歡迎來到長盈商標事務所</h1>
-                <p>這是首頁的內容。</p>
-                <p>請瀏覽我們的網站以了解更多信息。</p>
+                <div class="hero">
+                    <img src="/image/長盈LOGO.png" alt="Logo" className="logo" />
+                    <h1>歡迎來到長盈商標事務所</h1>
+                    <p>facebook</p>
+                    <p>Line</p>
+                    <p>這是首頁的內容。</p>
+                    <p>請瀏覽我們的網站以了解更多信息。</p>
+                </div>
 
             </div>
 
